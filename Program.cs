6cc73c1a661caf8f27cc3c231c10eb1d90cc63af
Geovane Jorge <geovane.jorge@gmail.com>
2022-09-08 19:46:01 -0300

    Estrutura For

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Poo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+	/*==============================================================================
+		//	Estrutura for
+			int numero = 5;
+			for (int contador = 0; contador <= 10; contador ++)
+			{
+				Console.WriteLine($"{numero} x {contador}= {numero * contador}");
+			}
+	=================================================================================*/
+
+		}
+
+	}
+}
