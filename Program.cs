50cf5c8e190d2fde3a17eafdf82d24b47dde8d1f
Geovane Jorge <geovane.jorge@gmail.com>
2022-09-08 21:58:06 -0300

    Estrutura While correção

--- Program.cs
+++ Program.cs
@@ -6,29 +6,25 @@ namespace Poo
 	{
 		static void Main(string[] args)
 		{
-			/*==============================================================================
-					//	Estrutura for
-						int numero = 5;
-						for (int contador = 0; contador <= 10; contador ++)
-						{
-							Console.WriteLine($"{numero} x {contador}= {numero * contador}");
-						}
-			=================================================================================*/
+			/*========================================================================
+			//Estrutura for
+			int numero = 5;
+			for (int contador = 0; contador <= 10; contador ++)
+				Console.WriteLine($"{numero} x {contador}= {numero * contador}");
+			
+			==========================================================================*/
+			/*=========================================================================
+			 //	Estrutura WHILE
 
+			int numero = 5;
+			int contador = 1;
+			while (contador <= 10)
+			{
+				Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+				contador++;
+			}
 
-
-	
-				//	Estrutura WHILE
-
-				int numero = 5;
-				int contador = 1;
-				while (contador <= 10)
-				{
-					Console.WriteLine($"{numero} x {contador} = {numero * contador}");
-					contador++;
-				}
-		
-
+			============================================================================*/
 
 
 		}
